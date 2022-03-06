33eba0e426a1a9676e4e67002ab0cea15998e15c
Breno Roosevelt <breno.jesus@ufms.br>
2022-03-06 17:13:24 -0400

    async await

--- src/service/PessoaService.js
+++ src/service/PessoaService.js
@@ -5,21 +5,11 @@ const ENDPOINT = '/pessoas'
 const ENDPOINT_ID = (id) => `${ENDPOINT}/${id}`
 
 export default class PessoaService {
-  /**
-   * @param page integer
-   * @param limit integer
-   * @param sort string
-   * @param sortDirection string
-   * @return Paginator
-   */
+
   static newPaginator(page = 1, limit = 15, sort = 'nome', sortDirection = 'asc') {
     return new Paginator(ENDPOINT, page, limit, sort, sortDirection)
   }
-
-  /**
-   * @param id string
-   * @return Promise
-   */
+  
   static get(id) {
     return http.get(ENDPOINT_ID(id))
   }

b3ed06702a291c701a0763f47454b832ef25c245
Breno Roosevelt <breno.jesus@ufms.br>
2022-03-06 17:41:29 -0400

    async await

--- src/model/Pessoa.js
+++ src/model/Pessoa.js
@@ -2,8 +2,8 @@ import Model from './Model'
 
 export default class Pessoa extends Model {
   constructor(id, nome) {
-    this._id = id;
-    this._nome = nome;
+    this.id = id;
+    this.nome = nome;
   }
 
   static fromBackEnd(data) {

22f39eb3911a4097d93f3e5b118e3ce8561e7a3b
Breno Roosevelt <breno.jesus@ufms.br>
2022-03-06 15:54:04 -0400

    service

--- src/service/PessoaService.js
+++ src/service/PessoaService.js
@@ -4,6 +4,14 @@ import Paginator from '../paginator'
 const ENDPOINT = '/pessoas/'
 
 export default class PessoaService {
+  /**
+   *
+   * @param page integer
+   * @param limit integer
+   * @param sort string
+   * @param sortDirection string
+   * @return Paginator
+   */
   static newPaginator(page = 1, limit = 15, sort = 'nome', sortDirection = 'asc') {
     return new Paginator(ENDPOINT, page, limit, sort, sortDirection)
   }

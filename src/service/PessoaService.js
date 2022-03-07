90bb28275cae3c1504b84ba9994dee3610414ef9
Breno Roosevelt <breno.jesus@ufms.br>
2022-03-06 20:24:53 -0400

    async await

--- src/service/PessoaService.js
+++ src/service/PessoaService.js
@@ -2,16 +2,17 @@ import { http } from '../http-common'
 import { FIRST_PAGE, DEFAULT_LIMIT, DEFAULT_DIRECTION, Paginator } from '../paginator'
 import Pessoa from '../model/Pessoa'
 
-const ENDPOINT = '/pessoas'
-const ofId = (id) => `${ENDPOINT}/${id}`
+const endpoint = '/pessoas'
+const ofId = (id) => `${endpoint}/${id}`
 
 export default class PessoaService {
   static newPaginator(page = FIRST_PAGE, limit = DEFAULT_LIMIT, sort = 'nome', direction = DEFAULT_DIRECTION) {
-    return (new Paginator(ENDPOINT, page, limit, sort, direction)).setParser((d) => Pessoa.fromData(d))
+    return (new Paginator(endpoint, page, limit, sort, direction)).setParser((d) => Pessoa.fromData(d))
   }
 
   static async get(id) {
     const { data } = await http.get(ofId(id))
+
     return Pessoa.fromData(data)
   }
 
@@ -19,7 +20,9 @@ export default class PessoaService {
     const postData = {
       nome: pessoa.nome
     }
-    const { data } = await http.post(ENDPOINT, postData)
+
+    const { data } = await http.post(endpoint, postData)
+
     return Pessoa.fromData(data)
   }
 }
